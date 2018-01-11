104baed91929946e6d0672999292c77f70174c99
kameronp <kameronp@lab1-11.eng.utah.edu>
2018-01-11 11:00:03 -0700

    Celestial body instantiation

--- calculator.c
+++ calculator.c
@@ -9,28 +9,44 @@ typedef struct{
 	float radius;
 }body;
 
+typedef struct{
+	float apoapsis;
+	float periapsis;
+	float eccentricity;
+	float semi_major_axis;
+	float period;
+	float ap_radii;
+	float pe_radii;
+	float GM;
+}orbit;
+
 const float G  = 6.67300E-11;
 void instantiate_bodies(body*);
+
 int main(int argc,const char* argv[])
 { 
 	body celestial_bodies[100];
 	instantiate_bodies(celestial_bodies);
-
-	float apoapsis;
-	float periapsis;
-	float satelite_mass;
-	float body_mass;
-	float T;
+	
+	body user_body;
+	orbit user_orbit;
 
 	printf("\nPlease Select Celestial Body:\n");
 	printf("0:Kerbol\n");
 	printf("1:Kerbin\n");
 	printf("2:Mun\n");
 	printf("3:Minmus\n");
+
 	int i;
 	scanf("%i",&i);
 	printf("You selected %s\n",celestial_bodies[i].name);
-	orbital_period(0.0,0.0,0.0);
+	
+	float desired_orbit;
+	printf("Please Enter Your Desired Orbit\n");
+	scanf("%f",&desired_orbit);
+	printf("You entered %f\n",desired_orbit);
+	user_body = celestial_bodies[i];
+	
 }
 
 float orbital_period(float mass, float apoapsis, float periapsis)
@@ -55,18 +71,19 @@ float eccentricity(float ap_radii,float pe_radii)
 
 void instantiate_bodies(body* bodies)
 {
+	//Kerbol
 	strcpy(bodies[0].name, "Kerbol");
 	bodies[0].mass = 0.0;
 	bodies[0].radius = 0.0;
-	
+	//Kerbin
 	strcpy(bodies[1].name,"Kerbin");
 	bodies[1].mass=0.0;
 	bodies[1].mass=0.0;
-
+	//Mun
 	strcpy(bodies[2].name, "Mun");
 	bodies[2].mass = 0.0;
 	bodies[2].radius = 0.0;
-	
+	//Minmus
 	strcpy(bodies[3].name, "Minmus");
 	bodies[3].mass=0.0;
 	bodies[3].radius=0.0;
